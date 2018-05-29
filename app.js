9eaa6e072aa152a598d5d6fa8ccb6475b693542e
fibbly <apsr9095@gmail.com>
2018-05-29 19:47:46 -0400

    added dependencies

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const port = process.env.PORT || 8000;
