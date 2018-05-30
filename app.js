0e5dfddcc53deb1fb5d093b17d065b66cc278d4f
fibbly <apsr9095@gmail.com>
2018-05-29 20:06:24 -0400

    server setup with mongoose

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 8000;
+const databaseURI = process.env.MONGODB_URI || 'mongodb://localhost/cards_against_humanity';
+
+mongoose.connect(databaseURI);
+const db = mongoose.connection;
+
+app.get('/', (req, res) => {
+    res.send('hi');
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
